1f920984a9bc2f8acfe2e2ccc9abc39a0218b46a
naverjsm <143307246+naverjsm@users.noreply.github.com>
2023-08-27 06:36:48 +0000

    Pending changes exported from your codespace

---
+++ script.js
@@ -0,0 +1,8 @@
+const h2 = document.querySelector("h2");
+const button = document.querySelector("button");
+
+console.log(button)
+button.addEventListener("click", () => {
+  const num = h2.innerText;
+  h2.innerText = Number(num) + 1;
+});
